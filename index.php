0998534cadcb6897c88ce7a2baa7ec50234d7e3e
mwstafford <mstafford@connextron.com>
2016-10-05 11:24:36 -0500

    create index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <title>biocode1</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>biocode1 </h1>
+        <?php
+                $line1 = 'The start of something great?';
+        ?>
+      <h2><?php echo $line1; ?></h2>
+      <p>This from github.</p>
+    </div>
+  </body>
+</html>
