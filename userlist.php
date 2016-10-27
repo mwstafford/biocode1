aaf4f2605b3788f0beaa025dd6b4cf0bddeaf02b
mwstafford <mstafford@connextron.com>
2016-10-27 16:50:42 -0500

    CI test

--- userlist.php
+++ userlist.php
@@ -38,7 +38,7 @@ foreach($items as $item){
 
 $user_table .= '</table>';
 echo $user_table;
-echo '<br><br><br>';
+echo '<br>t1<br><br>';
 
 ### SUBMIT NEW ENTRY
 if($_POST['submitapi'] == 'SUBMIT'){
